11f2377982be437d385a976eb113facedde1dde7
Anthony Gillet <amgillet@alum.wpi.edu>
2016-11-15 13:26:12 -0500

    .

--- src/player.rs
+++ src/player.rs
@@ -297,9 +297,13 @@ impl PlayerInternal {
                 }
                 Some(PlayerCommand::Pause) => {
                     self.update(|state| {
-                        state.status = PlayStatus::kPlayStatusPause;
-                        state.update_time = util::now_ms();
-                        state.position_ms = decoder.as_mut().map(|d| vorbis_time_tell_ms(d).unwrap()).unwrap_or(0) as u32;
+//                        state.status = PlayStatus::kPlayStatusPause;
+//                        state.update_time = util::now_ms();
+//                        state.position_ms = decoder.as_mut().map(|d| vorbis_time_tell_ms(d).unwrap()).unwrap_or(0) as u32;
+//                        state.position_measured_at = util::now_ms();
+                        state.status = PlayStatus::kPlayStatusStop;
+                        state.end_of_track = true;
+                        state.position_ms = 0;
                         state.position_measured_at = util::now_ms();
                         true
                     });
